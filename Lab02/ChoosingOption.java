8dd0dd1949b8189e38a138a67782d1888a991665
nnquang <nnquang0907@gmail.com>
2026-09-21 22:32:14 +0700

    complete assignment 4.1

--- Lab02/ChoosingOption.java
+++ Lab02/ChoosingOption.java
@@ -1,17 +1,12 @@
 package Lab02;
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class ChoosingOption {
     public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
-        System.out.println("What's your name?");
-        String strName = keyboard.nextLine();
-        System.out.println("How old are you?");
-        int iAge = keyboard.nextInt();
-        System.out.println("How tall are you ?(m)");
-        double dHeight = keyboard.nextDouble();
-        System.out.println("Mrs/Ms. " + strName + ", " + iAge +" years old." +
-                 " Your height is " + dHeight
+        int option = JOptionPane.showConfirmDialog(null,"Do you want to change the first class ticket?");
+        JOptionPane.showMessageDialog(null,"You've chosen: " +
+            (option == JOptionPane.YES_OPTION? "Yes":"No")
         );
+        System.exit(0);
     }
 }
