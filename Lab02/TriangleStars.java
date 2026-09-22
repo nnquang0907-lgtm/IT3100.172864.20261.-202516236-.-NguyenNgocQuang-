54e2d2a0155723ede299f301028556a9e1759798
nnquang <nnquang0907@gmail.com>
2026-09-22 20:05:20 +0700

    complete assignment 4.3

---
+++ Lab02/TriangleStars.java
@@ -0,0 +1,17 @@
+package Lab02;
+
+import java.util.Scanner;
+
+public class TriangleStars {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        int n = keyboard.nextInt();
+        for (int i = 1; i<=n; i++){
+            for (int j = 1; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
